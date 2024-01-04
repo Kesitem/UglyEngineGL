23da07c2518ba39d88c732d0c4335c285e2cf032
Kesitem <kesitem@gmail.com>
2024-01-04 11:23:57 +0100

    Draw a simple triangle

--- Samples/t01-SimpleTriangles/src/test_application.cpp
+++ Samples/t01-SimpleTriangles/src/test_application.cpp
@@ -27,20 +27,30 @@ void TestApplication::initialize()
         0.0f,  0.5f, 0.0f
     };
 
-    char vertex_shader_source[] = "#version 330 core\n"
+    const char *vertex_shader_source = "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
         "void main()\n"
         "{\n"
         "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
         "}\0";
 
-    char fragment_shader_source[] = "#version 330 core\n"
+    const char *fragment_shader_source = "#version 330 core\n"
         "out vec4 FragColor;\n"
         "void main()\n"
         "{\n"
         "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
         "}\0";
     
+    /*glGenVertexArrays(1, &m_va_id);
+    glBindVertexArray(m_va_id);
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);*/
+
+    m_va.bind();
+
     // Create vertex buffer
     auto bo = std::make_shared<ugly::VertexBuffer>(sizeof(vertices), vertices);
     bo->setLayout({ugly::BufferElement("a_vertex", ugly::BufferDataType::FLOAT3, false)});
@@ -71,4 +81,9 @@ void TestApplication::update()
 
     m_display_manager->setClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     m_display_manager->clear();
+
+    m_program.use();
+    m_va.bind();
+    m_display_manager->drawArrays(0, 3);
+    m_va.unbind();
 }
