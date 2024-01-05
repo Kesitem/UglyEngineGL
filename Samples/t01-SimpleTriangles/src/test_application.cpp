741ac2bebb67a775df9fa39f039e8d40ab327365
Kesitem <kesitem@gmail.com>
2024-01-05 11:23:08 +0100

    Draw a simple quad

--- Samples/t01-SimpleTriangles/src/test_application.cpp
+++ Samples/t01-SimpleTriangles/src/test_application.cpp
@@ -20,13 +20,6 @@ void TestApplication::initialize()
     m_input_manager->createButton("quit");
     m_input_manager->bindKeyToButton(GLFW_KEY_ESCAPE, "quit");
 
-    float vertices[] = 
-    {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    };
-
     const char *vertex_shader_source = "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
         "void main()\n"
@@ -40,24 +33,55 @@ void TestApplication::initialize()
         "{\n"
         "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
         "}\0";
-    
-    m_va.bind();
-
-    // Create vertex buffer
-    auto bo = std::make_shared<ugly::VertexBuffer>(sizeof(vertices), vertices);
-    bo->setLayout({ugly::BufferElement("a_vertex", ugly::BufferDataType::FLOAT3, false)});
-
-    // Set vertex pointer
-    m_va.addVertexBuffer(bo);
-
-    // Unbind vertex array
-    m_va.unbind();
 
     // Create program
     m_program.create(ugly::Shader(ugly::ShaderType::VERTEX, vertex_shader_source), 
                      ugly::Shader(ugly::ShaderType::FRAGMENT, fragment_shader_source));
 
-    
+    float vertices_simple_triangle[] = 
+    {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+
+    m_va_simple_triangle.bind();
+
+    // Create vertex buffer
+    auto bo = std::make_shared<ugly::VertexBuffer>(sizeof(vertices_simple_triangle), vertices_simple_triangle);
+    bo->setLayout({ugly::BufferElement("a_vertex", ugly::BufferDataType::FLOAT3, false)});
+
+    // Set vertex pointer
+    m_va_simple_triangle.addVertexBuffer(bo);
+
+    // Unbind vertex array
+    m_va_simple_triangle.unbind();
+
+    float vertices_quad[] = 
+    {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices_quad   [] = 
+    {  
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    }; 
+
+    m_va_quad.bind();
+
+    bo = std::make_shared<ugly::VertexBuffer>(sizeof(vertices_quad), vertices_quad);
+    bo->setLayout({ugly::BufferElement("a_vertex", ugly::BufferDataType::FLOAT3, false)});
+
+    m_va_quad.addVertexBuffer(bo);
+
+    auto ib = std::make_shared<ugly::IndexBuffer>((uint32_t)sizeof(indices_quad), indices_quad);
+
+    m_va_quad.setIndexBuffer(ib);
+
+    m_va_quad.unbind();
 }
 
 
@@ -71,27 +95,71 @@ void TestApplication::update()
     if(m_input_manager->getButtonAction("quit") == ugly::InputAction::released)
         m_engine->quit();
 
+    switch (m_render_mode)
+    {
+    case 0:
+        m_display_manager->setPolygonMode(ugly::DisplayManager::PolygonMode::LINE);
+        break;
+    case 1:
+        m_display_manager->setPolygonMode(ugly::DisplayManager::PolygonMode::FILL);
+        break;
+    }
+
     m_display_manager->setClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     m_display_manager->clear();
 
-    m_program.use();
-    m_va.bind();
-    m_display_manager->drawArrays(0, 3);
-    m_va.unbind();
+    if(m_sample == 0)
+    {
+        m_program.use();
+        m_va_simple_triangle.bind();
+        m_display_manager->drawArrays(0, 3);
+        m_va_simple_triangle.unbind();
+    } 
+    else if (m_sample == 1)
+    {
+        m_program.use();
+        m_va_quad.bind();
+        m_display_manager->drawElements(6);
+        m_va_quad.unbind();
+    }
 }
 
 
 void TestApplication::updateImgui()
 {
-    ImGui::ShowDemoWindow();
-
-    static bool test1 = true;
-    static bool test2 = false;
+    static std::vector<std::string> sample_list({ "simple triangle", "simple quad", "2 triangles", "2 triangles 2 VA", "2 triangles 2 VA 2 Programs"});
+    static std::vector<std::string> render_mode_list({ "line", "fill" });
     ImGui::Begin("Options");
     {
-        ImGui::Text("This is some useful text."); 
-        ImGui::Checkbox("Demo Window", &test1);      // Edit bools storing our window open/close state
-        ImGui::Checkbox("Another Window", &test2);
+        if (ImGui::BeginListBox("Sample"))
+        {
+            for (int n = 0; n < sample_list.size(); n++)
+            {
+                const bool is_selected = (m_sample == n);
+                if (ImGui::Selectable(sample_list[n].c_str(), is_selected))
+                    m_sample = n;
+
+                // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+                if (is_selected)
+                    ImGui::SetItemDefaultFocus();
+            }
+             ImGui::EndListBox();
+        }
+
+        if (ImGui::BeginListBox("Render mode"))
+        {
+            for (int n = 0; n < render_mode_list.size(); n++)
+            {
+                const bool is_selected = (m_render_mode == n);
+                if (ImGui::Selectable(render_mode_list[n].c_str(), is_selected))
+                    m_render_mode = n;
+
+                // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+                if (is_selected)
+                    ImGui::SetItemDefaultFocus();
+            }
+            ImGui::EndListBox();
+        }
     }
     ImGui::End();
 }
--- Samples/t01-SimpleTriangles/src/test_application.h
+++ Samples/t01-SimpleTriangles/src/test_application.h
@@ -45,7 +45,18 @@ private:
     /*! Display manager */
     ugly::DisplayManager* m_display_manager {nullptr};
 
-    ugly::VertexArrays m_va;
+    /*! Shader program*/
+    ugly::Program m_program;
 
-	ugly::Program m_program;
+    /*! Vertex array for simple triangle */
+    ugly::VertexArrays m_va_simple_triangle;
+
+    /*! Vertex array for the quad */
+    ugly::VertexArrays m_va_quad;
+
+    /*! Selected sample */
+    int m_sample {0};
+
+    /*! Render mode (wireframe or fill) */
+    int m_render_mode{ 1 };
 };
